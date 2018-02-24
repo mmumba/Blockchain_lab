e993728254e844a851362b42791564b4be816a52
mmumba <mambwemumba@gmail.com>
2018-02-24 12:10:40 -0800

    smart contract tutorial

---
+++ hello_world_voting/Voting.sol
@@ -0,0 +1,34 @@
+  */
+  
+  bytes32[] public candidateList;
+
+  /* This is the constructor which will be called once when you
+  deploy the contract to the blockchain. When we deploy the contract,
+  we will pass an array of candidates who will be contesting in the election
+  */
+  function Voting(bytes32[] candidateNames) public {
+    candidateList = candidateNames;
+  }
+
+  // This function returns the total votes a candidate has received so far
+  function totalVotesFor(bytes32 candidate) view public returns (uint8) {
+    require(validCandidate(candidate));
+    return votesReceived[candidate];
+  }
+
+  // This function increments the vote count for the specified candidate. This
+  // is equivalent to casting a vote
+  function voteForCandidate(bytes32 candidate) public {
+    require(validCandidate(candidate));
+    votesReceived[candidate] += 1;
+  }
+
+  function validCandidate(bytes32 candidate) view public returns (bool) {
+    for(uint i = 0; i < candidateList.length; i++) {
+      if (candidateList[i] == candidate) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
